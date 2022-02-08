173765759fa66ad5fef9c1b5eadb8641555b2089
antoniocukrov <antonio.cukrov1@gmail.com>
2022-02-08 12:44:18 +0100

    Login GUI
    Panel i frame

---
+++ LoginGUI/src/main/java/Login.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Login {
+        
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(300,250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+               
+        JPanel panel = new JPanel();
+        
+    }
+     
+    
+}
