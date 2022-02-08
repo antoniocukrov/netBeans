f8bb208485d51f394e3375bffa635dfffad5d2bc
antoniocukrov <antonio.cukrov1@gmail.com>
2022-02-08 11:51:26 +0100

    Fixing the label

--- FirstButton/src/main/java/button/GUI.java
+++ FirstButton/src/main/java/button/GUI.java
@@ -17,20 +17,21 @@ import javax.swing.JPanel;
  */
 public class GUI implements ActionListener {
     private int count = 0;
-    private JLabel label;
-    private JFrame frame;
-    private JPanel panel;
+    private final JLabel label;
+    private final JFrame frame;
+    private final JPanel panel;
+    private final JButton button;
     
     public GUI() {
         frame = new JFrame();
         
-        JButton button = new JButton("Click me");
+        button = new JButton("Click me");
         button.addActionListener(this);
         
-        label = new JLabel("Number of clicks:0");
+        label = new JLabel("Number of clicks:0 ");
         
         panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setBorder(BorderFactory.createEmptyBorder(30,50,10,50));
         panel.setLayout(new GridLayout(0,1));
         panel.add(button);
         panel.add(label);
@@ -39,7 +40,8 @@ public class GUI implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Our GUI");
         frame.pack();
-        frame.setVisible(true);    } 
+        frame.setVisible(true);   
+    } 
     
     public static void main(String[] args) {
         new GUI();
